bc23bfb804521bbd7ea022f60879af126082049a
JaydenMikulcik <jjmikulcik@gmail.com>
2023-02-27 11:36:23 -0700

    here

--- src/components/leftSide.jsx
+++ src/components/leftSide.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import Table from "react-bootstrap/Table";
 import Cards from "./notecard";
@@ -6,6 +7,18 @@ import "../styles/styles.css";
 
 function LeftSide() {
   const [data, setData] = useState([]);
+
+  let addNewCard = () => {
+    let entries = JSON.parse(localStorage.getItem("entries")) || [];
+    entries.push({ Objtitle: "", Objbody: "", ObjId: entries.length });
+    let addnewRoute = "/notes/" + (entries.length - 1);
+    console.log(entries.length);
+
+    localStorage.setItem("entries", JSON.stringify(entries));
+    console.log(addnewRoute);
+
+    return addnewRoute;
+  };
 
   useEffect(() => {
     const storedData = localStorage.getItem("entries");
@@ -20,7 +33,11 @@ function LeftSide() {
         <thead className="topPart">
           <tr>
             <th>Notes</th>
-            <th>+</th>
+            <th>
+              <Link to={addNewCard}>
+                <div onClick={addNewCard}>+</div>
+              </Link>
+            </th>
           </tr>
         </thead>
         <tbody>
